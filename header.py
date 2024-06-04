84b0ad346b5899525965ba6a3aae7c49743b5e03
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2024-06-04 14:38:46 -0500

    Update header.py

--- header.py
+++ header.py
@@ -15,6 +15,7 @@ def create_header():
             width: 100%;
         }
         .header img {
+          
             animation: smokeDisappear 2s ease-in-out forwards;
         }
 
@@ -33,9 +34,12 @@ def create_header():
     """, unsafe_allow_html=True)
 
     st.markdown("<div class='header'>", unsafe_allow_html=True)
-    st.image("Genie-ome.png", width=180)
-    st.markdown("</div>", unsafe_allow_html=True)
-    st.markdown("<div class='title'>Genie-ome: A Genome Geolocator Model</div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtitle'>By Arjun, Daren, Sarina, Pooja</div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtitle'>Upload a .fasta file, let's predict which region of the world it comes from</div>", unsafe_allow_html=True)
+    col1, col2 = st.columns([1,8])
+
+    with col1:
+      st.image(image_path, width=180)
+    with col2:
+        st.markdown("<div class='title'>Genie-ome: A Genome Geolocator Model</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>By Arjun, Daren, Sarina, Pooja</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>Upload a .fasta file, let's predict which region of the world it comes from</div>", unsafe_allow_html=True)
     st.markdown("</div>", unsafe_allow_html=True)
